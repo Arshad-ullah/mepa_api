5cdd395ea7450496680efebd9af813e1159b2213
Arshad ullah <jahan665577@gmail.com>
2026-04-23 10:43:57 +0500

    changes done

--- public/array.js
+++ public/array.js
@@ -3,7 +3,23 @@ let names = ["Arshad", "Jahan", "Gul"]
 
 
 
-names.unshift("Wali khan")
+// names.unshift("Wali khan")
+// names.push("jahan")
+
+// delete element
+
+
+
+// shift(remove in index 0). pop remove in the end 
+
+// names.shift()
+// names.pop()
+
+// now in the middle so we need to use splice have two paramters index and value
+
+
+
+names.splice(0, 1, "janm,d.ajkl", "Ikram")
 console.log(names);
 
 
