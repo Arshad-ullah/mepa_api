5c9fdc7da0db1f99935d97d03f8aea0b017e66fc
Arshad ullah <jahan665577@gmail.com>
2026-04-23 09:14:36 +0500

    nested array destruction

--- public/dest.js
+++ public/dest.js
@@ -1,4 +1,18 @@
-const [name, _, age] = ["jahan", 23]
+// const [name, , age] = ["jahan", "", 24]
+
+
+// console.log(name + "\t" + age);
+
+
+
+// Nested array destruction array..
+
+
+const [name, [district, vallige], age] = ["Arshad", ["Hangu", "Karbogha sharif"], 23]
 
 
 console.log(name);
+
+console.log(district);
+console.log(vallige)
+
