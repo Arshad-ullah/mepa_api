bec33369336cff357bf7be8cb7a46a36c28c1007
Arshad ullah <jahan665577@gmail.com>
2026-04-27 09:02:15 +0500

    post json added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express=require('express');
+const express = require('express');
 
-const authRouter=require('./routes/auth')
+const authRouter = require('./routes/auth')
+const data = require('./post.json')
 
-const app=express();
+
+
+const app = express();
 
 
 app.use(express.json())
-app.use('/v1',authRouter.router)
+app.use('/v1', authRouter.router)
 
 
-app.listen(8000,()=>console.log("server started"))
+console.log(data);
+
+
+
+app.listen(8000, () => console.log("server started"))
