7c7ed2b4f88f41d9d2ca259889aa0986b8893aba
Arshad ullah <jahan665577@gmail.com>
2026-05-06 16:20:05 +0500

    changes done

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,11 @@ const hashPassword = require('../hash_password')
 
 exports.login = async (req = Request, res = Response) => {
 
+    const password = req.body.password;
 
-
-    const hash_password = await hashPassword.hashPassword1()
-
-
+    const hash_password = await hashPassword.hashPassword1(
+        password
+    )
     res.status(201).json({
         message: "Login api",
         "hashpassword": hash_password,
--- hash_password.js
+++ hash_password.js
@@ -2,16 +2,31 @@ const bcrypt = require('bcrypt')
 
 const saltRounds = 10
 
-const userPassword = "arshad123"
 
 
 
-async function hashPassword1() {
+
+async function hashPassword1(userPassword) {
+
+
+
 
     return await bcrypt.hash(userPassword, saltRounds);
 
 }
 
+
+async function comparePassword(userPassword) {
+
+    const value = await bcrypt.compare(userPassword, "$2b$10$4Oaz/gLHh//1ZAVLKT/COuM/TPzMa0MjzB.zXVCo5Ecl8dJvmM1BK")
+
+    console.log("value.." + value);
+
+
+}
+
+
 module.exports = {
-    hashPassword1
+    hashPassword1,
+    comparePassword,
 };
