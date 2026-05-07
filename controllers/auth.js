d1bac7d0f97f41dda43bac822168f706f5b70665
Arshad ullah <jahan665577@gmail.com>
2026-05-07 09:08:53 +0500

    changes done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,10 @@
-const { Request, Response } = require('express')
 
 const hashPassword = require('../hash_password')
-exports.login = async (req = Request, res = Response) => {
-
+const { Request, Response, request, response } = require('express')
+exports.login = async (req = request, res = response) => {
 
     const { email, password } = req.body.password;
     const hash_password = await hashPassword.comparePassword(password)
-
 
     res.status(201).json({
         message: "Login api",
@@ -15,4 +13,27 @@ exports.login = async (req = Request, res = Response) => {
     })
 }
 
+
+
+
+// register api...
+
+
+//name,fatherName, email,password,phone, address, gender,University, department,Roll_no,
+exports.register = (req = request, res = response) => {
+
+
+
+
+
+
+
+
+
+    const { name, fatherName, email, phone, address, gender, university, department, rollNo } = req.body
+
+
+
+
+}
 
