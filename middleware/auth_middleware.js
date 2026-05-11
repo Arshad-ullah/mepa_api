0e3de16db4590994905d006f8f6da8788276614c
Arshad ullah <jahan665577@gmail.com>
2026-05-11 09:52:38 +0500

    changes done

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -3,6 +3,8 @@ const { validationResult } = require('express-validator');
 const validate = (req, res, next) => {
     const errors = validationResult(req);
 
+
+
     if (!errors.isEmpty()) {
         return res.status(400).json({
             success: false,
